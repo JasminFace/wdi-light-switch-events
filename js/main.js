93ff7a9f689c93d022e68b00af880a8a49d0455b
Jasmin Saromo <jasminsaromo@gmail.com>
2019-06-18 22:33:56 -0400

    completed assignment

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
 
+    const button = document.querySelector('button')
+    const body = document.querySelector('body')
+    const status = document.querySelector('.status')
 
+    function lightSwitch() {
+        if (button.classList.contains('on')) {
+            button.classList.remove('on');
+            button.classList.add('off');
+            body.classList.remove('light');
+            body.classList.add('dark');
+            status.innerText = 'Hey, who turned off the lights?';
+        } else {
+            button.classList.remove('off');
+            button.classList.add('on');
+            body.classList.remove('dark');
+            body.classList.add('light');
+            status.innerText = "It's so bright in here!";
+        }
+    }
+
+    button.addEventListener('click', lightSwitch);
+
+});
